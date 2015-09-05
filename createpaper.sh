5e24a4cefd7b5aa5e8f02a3345ec81c59b46d115
Kassius <kassiv.s@gmail.com>
2015-09-05 10:28:00 -0300

    Only one file

--- createpaper.sh
+++ createpaper.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]
+if [ "$#" -lt 1 ]
 then
-	echo "Use: bash createpaper.sh <FILENAME>"
+	echo "Use: bash createpaper.sh <FILENAME> [--transparency]"
 	exit -1;
+fi
+
+FILENAME=$1
+
+while test $# -gt 0
+do
+	case "$1" in
+		--transparency) TRANSPARENCY=true
+			;;
+	esac
+	shift
+done
+
+if [ ! $TRANSPARENCY ]
+then
+		INPUT_MODEL="$HOME/Documents/Letterheads/letterhead-final.png"
+else
+		INPUT_MODEL="$HOME/Documents/Letterheads/letterhead-transparent-final.png"
 fi
 
 convert 											\
 												\
-		~/Documents/Letterheads/letterhead-final.png					\
+		$INPUT_MODEL									\
 												\
 		-font '/usr/share/fonts/truetype/inconsolata/Inconsolata.otf'			\
 		-fill '#0008'									\
@@ -18,13 +36,13 @@ convert 											\
 		-interline-spacing -5								\
 		-annotate +36+97 '@-'								\
 												\
-												\
-		"$1"
-if [ -e "$1" ]
+		"$FILENAME"
+
+if [ -e "$FILENAME" ]
 then
-	echo "$1 created successfully!"
+	echo "$FILENAME created successfully!"
 	exit 0
 else
-	echo "error creating $1"
+	echo "error creating $FILENAME"
 	exit -2
 fi
